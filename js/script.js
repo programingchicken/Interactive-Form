44b9d90b9842a98e9925b126e8cdf08567bece15
programingchicken <aquacrazz.gp@gmail.com>
2021-12-29 19:21:01 -0800

    fixed comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//variables for elements
 const name = document.getElementById('name');
 const design = document.getElementById('design');
 const color = document.getElementById('shirt-colors');
@@ -7,6 +8,9 @@ const otherJobRole = document.getElementById('other-job-role');
 const registerForActivites = document.getElementById('activities-box');
 let cost = 0;
 const total$ = document.getElementById('activities-cost');
+const form = document.getElementById('form')
+
+//pay variables elements
 const creditCard = document.getElementById('credit-card');
 const paypal = document.getElementById('paypal');
 const bitcoin = document.getElementById('bitcoin');
@@ -15,41 +19,44 @@ creditCard.style.display = "none";
 paypal.style.display = "none";
 bitcoin.style.display = "none";
 const creditSpan = document.getElementById('cc-hint');
-const zipSpan = document.getElementById('zip-hint') 
+const zipSpan = document.getElementById('zip-hint')
 const cvvSpan = document.getElementById('cvv-hint')
 
+//email elements
 const email = document.getElementById('email');
 const emailSpan = document.getElementById('email-hint');
-
 const nameSpan = document.getElementById('name-hint');
 
+
+//name hint
 name.addEventListener('input', (e) => {
-     const nameEvent = e.target;
-     if (nameEvent.getAttribute('type') === 'text') {
-         if (nameEvent.value === "") {
+    const nameEvent = e.target;
+    if (nameEvent.getAttribute('type') === 'text') {
+        if (nameEvent.value === "") {
             nameSpan.className = "name";
-         } else {
-             nameSpan.className = 'name-hint hint'
-         }
-     }
+        } else {
+            nameSpan.className = 'name-hint hint'
+        }
+    }
 })
 
+//email hint
 email.addEventListener('input', (e) => {
     const emailInput = e.target
     if (emailInput.getAttribute('type') === 'email') {
         if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(emailInput.value)) {
             emailSpan.className = 'email-hint hint';
-        }else {
+        } else {
             emailSpan.className = 'email';
         }
 
     }
 });
 
-
+//payment hint credit card
 payment.addEventListener('change', (e) => {
     const paymentEvent = e.target;
-    if (paymentEvent.value === 'credit-card' ) {
+    if (paymentEvent.value === 'credit-card') {
         creditCard.style.display = "";
         paypal.style.display = "none";
         bitcoin.style.display = "none";
@@ -64,42 +71,42 @@ payment.addEventListener('change', (e) => {
     } else {
         console.log('error');
     }
-}) 
+})
 
 
 
-
+//payment hint debit card
 creditCard.addEventListener('input', (e) => {
     const creditCardEvent = e.target;
     if (creditCardEvent.getAttribute('id') === "exp-month") {
-console.log(1)
-    } else if ( creditCardEvent.getAttribute('id') === 'exp-year') {
-console.log(2)
+        console.log(1)
+    } else if (creditCardEvent.getAttribute('id') === 'exp-year') {
+        console.log(2)
     } else if (creditCardEvent.getAttribute('id') === 'cc-num') {
         if (/^([0-9]{13,16})$/.test(creditCardEvent.value)) { //fix
             creditSpan.className = 'cc-hint hint';
-        }else if (creditCardEvent.value === '') {
+        } else if (creditCardEvent.value === '') {
             creditSpan.className = 'cc-hint hint';
         } else {
-            creditSpan.className = 'cc';  
+            creditSpan.className = 'cc';
         }
         console.log(3)
     } else if (creditCardEvent.getAttribute('id') === 'zip') {
-        if (/^([0-9]{5})$/.test(creditCardEvent.value)){
+        if (/^([0-9]{5})$/.test(creditCardEvent.value)) {
             zipSpan.className = 'zip-hint hint';
         } else if (creditCardEvent.value === '') {
             zipSpan.className = 'zip-hint hint';
         } else {
-        zipSpan.className = 'zip';
+            zipSpan.className = 'zip';
         }
         console.log(4)
     } else if (creditCardEvent.getAttribute('id') === 'cvv') {
-        if (/^([0-9]{3})$/.test(creditCardEvent.value)){
+        if (/^([0-9]{3})$/.test(creditCardEvent.value)) {
             cvvSpan.className = 'cvv-hint hint';
         } else if (creditCardEvent.value === '') {
             cvvSpan.className = 'cvv-hint hint';
         } else {
-        cvvSpan.className = 'cvv';
+            cvvSpan.className = 'cvv';
         }
         console.log(5)
     }
@@ -107,12 +114,12 @@ console.log(2)
 
 
 
-
+//Activity check box for adding how much it cost
 registerForActivites.addEventListener('change', (e) => {
-    checkboxs= e.target
-    let realCost = 0 ;
+    checkboxs = e.target
+    let realCost = 0;
     if (checkboxs.type === 'checkbox') {
-        if(checkboxs.checked) {
+        if (checkboxs.checked) {
             cost += realCost + parseInt(checkboxs.getAttribute('data-cost'));
             console.log(cost)
             total$.textContent = `Total: $${cost}.00`
@@ -122,7 +129,7 @@ registerForActivites.addEventListener('change', (e) => {
             total$.textContent = `Total: $${cost}.00`
         }
     }
-}) ;
+});
 
 
 
@@ -135,33 +142,42 @@ console.log(name)
 otherJobRole.style.display = "none";
 color.style.display = "none";
 
-design.addEventListener('change', (e) =>{
+//defferent disigns for shirts
+design.addEventListener('change', (e) => {
     const designEvent = e.target;
     const colorText = document.getElementById('color').textContent
     if (designEvent.textContent !== 'Select Theme') {
         color.style.display = "";
 
         for (i = 1; i <= colorChild.length - 1; i++) {
-           const valid = colorChild[i].getAttribute('data-theme');
-           valueDesign = designEvent.value;
-           if (designEvent.value === valid) {
-            colorChild[i].style.display = "";
-           } else {
-            colorChild[i].style.display = "none";
-           }
+            const valid = colorChild[i].getAttribute('data-theme');
+            valueDesign = designEvent.value;
+            if (designEvent.value === valid) {
+                colorChild[i].style.display = "";
+            } else {
+                colorChild[i].style.display = "none";
+            }
         }
     }
 });
 
+//job-role text box
 jobRole.addEventListener('change', (e) => {
     const jobRoleEvent = e.target;
     if (jobRoleEvent.value === 'other') {
         otherJobRole.style.display = "";
-    }else {
+    } else {
         otherJobRole.style.display = "none";
     }
 });
 
+//submit 
+form.addEventListener('click', (e) => {
+    if (e.target.id === "submit") {
+        console.log('hi')
+        form.innerHTML = "You Completed The Form!!!"
+    }
+})
 
 console.log(jobRole.value)
 console.log(otherJobRole);
